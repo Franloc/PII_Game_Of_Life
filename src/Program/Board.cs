a0e6c401ce49e2c5e2a5363749a496ec4b9daad4
Nicolas Bascou <nicolas.bascou@correo.ucu.edu.uy>
2026-04-08 21:08:40 -0300

    version preeliminar de program y board

---
+++ src/Program/Board.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Board
+{
+    private List<Cell> cells = new List<Cell>();
+    public List<Cell> Cells
+    {
+        get { return this.cells;} set { this.cells = value;}
+    }
+
+    public Board(List<CboardCells)
+    {
+        this.cells = 
+    }
+
+}
